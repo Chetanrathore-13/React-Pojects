5ccd8ddf69fbc9ab404f6f379e642cfb200019f2
Chetan Rathore <154863317+Chetan9131@users.noreply.github.com>
2024-08-17 11:49:46 +0530

    react-router-dom practice

---
+++ react-router-dom/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='bg-blue-700 text-white p-4'>Waheguru</h1>
+    </>
+  )
+}
+
+export default App
---
+++ react-router-dom/src/main.jsx
@@ -0,0 +1,41 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import Layout from './Layout'
+import Home from './components/Home/Home'
+import App from './App'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import Github from './components/Github/Github'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children:[
+      {
+        path:"",
+        element: <Home/>
+      },
+      {
+        path:"about",
+        element: <About/>
+      },
+      {
+        path:"contact",
+        element: <Contact/>
+      },
+      {
+        path:"github",
+        element: <Github/>
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
