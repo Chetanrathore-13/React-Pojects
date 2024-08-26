6a3d13c1fd275df2b8a6491274e26436629675c5
Chetan Rathore <crathore9131@gmail.com>
2024-08-26 17:35:59 +0530

    Delete blog-website directory

--- blog-website/src/config/config.js
+++
@@ -1,9 +0,0 @@
-const conf = {
-     appwriteURl: String(import.meta.env.VITE_APPWRITE_URL),
-     appwriteProjectID: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-     appwriteDatabseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-}
-
-export default conf;
